1a61be4a58e6f3b9eb3459734899de5bb3aa33a4
Hamza Khuswan <hamzakhuswan@outlook.com>
2020-12-23 18:28:16 +0100

    clouds

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import light from "./main/light";
 
 // Meshes
 import loadEarth from "./mesh/earth"
+import loadClouds from './mesh/clouds';
+import { Mesh } from 'three';
 
 // Helpers
 // import helpers from "./helpers";
@@ -22,16 +24,22 @@ const spinner = document.querySelector("div.spinner");
 const canvas = document.querySelector("canvas");
 
 // Main 
-let earth;
-loadEarth()
-    .then(obj => earth = obj)
+let objects = [];
+//Earth, clouds, atompshere
+Promise.all([loadEarth(), loadClouds()])
+    .then(objs => objects = objs)
     .catch(err => console.log(err))
     .finally(() => main());
 
+
+let earth, clouds, atompshere;
 function main() {
+    earth = objects[0];
+    clouds = objects[1];
+
     // Add Stuff
     scene.add(light);
-    scene.add(earth);
+    objects.map(obj => scene.add(obj));
 
     // Event listners
     window.addEventListener("resize", () => {
@@ -59,6 +67,10 @@ orbit.minDistance = 4;
 
 // Start animation
 const animate = () => {
+    earth.rotation.y += 0.0005;
+    clouds.rotation.y -= 0.002;
+
+
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
 
---
+++ src/mesh/clouds.js
@@ -0,0 +1,21 @@
+
+import * as THREE from "three";
+import config from "../../config";
+
+const TextureLoader = new THREE.TextureLoader;
+
+const loadClouds = () => new Promise(async (resolve, reject) => {
+    const cloudsGeometry = new THREE.SphereGeometry((config.earthsRadius || 2) * 1.005, 32, 32);
+    const cloudsMaterial = new THREE.MeshPhysicalMaterial({ color: 0xffffff });
+
+    const cloudsTexture = await TextureLoader.loadAsync("./textures/Clouds.png");
+    cloudsMaterial.alphaMap = cloudsTexture;
+    cloudsMaterial.transparent = true;
+    cloudsMaterial.depthWrite = false;
+
+    const clouds = new THREE.Mesh(cloudsGeometry, cloudsMaterial);
+
+    resolve(clouds)
+});
+
+export default loadClouds;
